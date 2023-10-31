00a63c4f133dbf82fc88ff22a10977cf9c1fb476
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-10-31 15:29:21 -0500

    edited webpack and service workers

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate  } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// implemented StaleWhileRevalidate, this strategy will first check the cache for a response, and if it finds one, it will return it.
+registerRoute( ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  // Name of the cache storage.
+  cacheName: 'asset-cache',
+  plugins: [
+    // This plugin will cache responses with these headers to a maximum-age of 30 days
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+})
+);
